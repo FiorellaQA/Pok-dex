0edf6a689d28a2766e355e17f9936274bda66163
FiorellaQA <fiorella_90_17@hotmail.com>
2017-06-15 18:07:20 -0500

    ingresando funcionalidad

--- js/app.js
+++ js/app.js
@@ -1,18 +1,52 @@
 'use strict';
 
-function render(root) {
-	root.empty();
-	var wrapper = $('<div>',{
-		class: 'wrapper'
-	});
-	wrapper
-		.append(header())
-		.append(search());
 
-	root.append(wrapper);
-}
 
 $(function() {
-	var root = $('#root');
-	render(root);
+	$.getJSON("http://pokeapi.co/api/v2/pokedex/1/", function (response) {
+			var pokemons = response.pokemon_entries;
+			item(pokemons);
+		});
 });
+
+function item(pokemons) {
+	var i = 0;
+	pokemons.forEach(function (pokemon) {
+ 	i++;
+	var col = $('<div>',{
+		class: 'col-xs-6 col-md-3'
+	});
+
+	var anchor = $('<a>',{
+		href:'#',
+		class: 'thumbnail'
+	});
+
+	var name = $('<p>',{
+		html:pokemon.pokemon_species.name
+	});
+
+	var img = $('<img>',{
+		src:'http://serebii.net/art/th/' + i + '.png',
+		alt:pokemon.pokemon_species.name + ' Logo'
+	});
+
+	anchor.append(img);
+	col
+		.append(anchor)
+		.append(name);
+
+	$('#print').append(col);
+	});
+}
+
+	/* $.ajax({
+		url:'http://pokeapi.co/api/v2/pokedex/1/',
+	 $(function() {
+
+
+
+
+
+	*/
+
--- js/components/header.js
+++
@@ -1,28 +0,0 @@
-'use strict';
-
-function header () {
-	var container = $('<header>',{
-		class: 'container-fluid',
-		id:'app-header'
-	});
-
-	var row = $('<div>',{
-		class: 'row'
-	});
-
-	var col = $('<div>',{
-		class: 'col-xs-12 text-center'
-	});
-
-	var title = $('<h1>',{ 
-		html:'Pokédex' ,
-		class:'title'
-	});  
-
-	col.append(title);
-	row.append(col);
-	container.append(row);
-
-	return container;
-}
-
